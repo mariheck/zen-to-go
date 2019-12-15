d6b0dee35a39e05fdb8325d42ca2bf79e0b779c5
Marine Heckler <marine.heckler@gmail.com>
2019-12-15 16:27:19 +0100

    Complete the content in both languages.

--- content.js
+++ content.js
@@ -9,9 +9,10 @@ exports.getCzContent = function() {
       "Vše, co jste chtěli vědět o józe, ale báli se zeptat.",
     ],
     navbar: [
-      "About",
-      "Offer",
-      "Contact",
+      "Intro",
+      "Ceny",
+      "O Mně",
+      "Namaste",
       "/en",
       "en/cz"
     ],
@@ -19,7 +20,7 @@ exports.getCzContent = function() {
       "vše, co jste chtěli vědět o józe, ",
       "ale báli se zeptat"
     ],
-    about: [
+    intro: [
       "Vinyasa / Hatha Flow",
       "Pranayama",
       "Meditace",
@@ -30,22 +31,28 @@ exports.getCzContent = function() {
       "Je jedno, jestli jste jógu v životě nezkusili a nevíte jak začít, nebo se bojíte, že nejste dost silní a flexibilní.",
       "Jóga je pro každého. A každý, kdo se jí věnuje bez očekávání výsledků, získá tu největší odměnu - spojení těla, mysli a duše."
     ],
-    offer: [
+    prices: [
       "Individualní Lekce",
-      "Choose your class!",
+      "Ceny",
       "60 min - 700 Kč",
       "90 min - 900 Kč",
       "Lekce na míru, individuální přístup. ",
       "90 min lekce jsou vhodné i pro 2 osoby."
     ],
+    about: [
+      "O Mně",
+      "Jmenuju se Anastasia a moje cesta k józe začala před 13 léty, kdy mě maminka přivedla na lekci ashtanga vinyasa jógy, abych něco dělala se skoliózou. Nebyla to láska na první pohled, ale to u jógy bývá. Na začátku prostě nevěříte, že pes hlavou dolů je relaxační pozice. :)",
+      "Nejsem rigidní v praxi ani výuce jógy, fascinují mě odlišné styly, anatomie jógy, práce s dechem a energií, meditační techniky a v neposlední řadě filosofie a mytologie.",
+      "Učit jsem začala v roce 2019 po dokončení učitelského kurzu v Ashiyana yoga school (http://www.ashiyanayogaschool.in/) v Indii. To, co mě na výuce baví je individuální práce se studenty, adjustmenty a vytvoření empatického prostředí. Proto učím inividuální lekce a malé skupiny."
+    ],
     contact: [
       "/#contact",
       "Jméno",
       "Email",
-      "Subject",
-      "Type your message here...",
+      "Předmět",
+      "Zde napište svou zprávu...",
       "Poslat",
-      "Poslal !"
+      "Poslal!"
     ],
     footer: "Website made by"
   };
@@ -64,9 +71,10 @@ exports.getEnContent = function() {
       "Everything you wanted to know about yoga, but were afraid to ask",
     ],
     navbar: [
-      "About",
-      "Offer",
-      "Contact",
+      "Intro",
+      "Prices",
+      "About Me",
+      "Namaste",
       "/",
       "en/cz"
     ],
@@ -74,24 +82,30 @@ exports.getEnContent = function() {
       "everything you wanted to know about yoga, ",
       "but were afraid to ask"
     ],
-    about: [
+    intro: [
       "Vinyasa / Hatha Flow",
       "Pranayama",
-      "Meditace",
-      "Chcete na chvíli pozastavit mysl a naučit se vnímat přítomný okamžik?",
-      "Nebo Vás bolí záda od sezení u počítače a chcete se protáhnout?",
-      "Chcete začít meditovat, abyste byli o 10% šťastnější a nevíte, jak začít?",
-      "Ať už je důvod jakýkoliv, ozvěte se, a připravím Vám praxi na míru. Individuální praxe i mini skupiny.",
-      "Je jedno, jestli jste jógu v životě nezkusili a nevíte jak začít, nebo se bojíte, že nejste dost silní a flexibilní.",
-      "Jóga je pro každého. A každý, kdo se jí věnuje bez očekávání výsledků, získá tu největší odměnu - spojení těla, mysli a duše."
+      "Meditation",
+      "Do you want to stop the fluctuations of your mind and be present?",
+      "Or is it your back pain and feeling of stiffness that makes you consider yoga as a solution?",
+      "Maybe meditation is your goal and you want to learn simple daily techniques?",
+      "Whatever is the reason, reach out to me and I will create safe and nurturing yoga space for you.",
+      "Doesn’t matter if you’ve never tried yoga before and you don’t know where to start, also don’t worried about not being strong and flexible enough.",
+      "Yoga is for everyone. If you practice without expecting results, you will get the biggest reward – transformation and union of your body, mind and soul."
     ],
-    offer: [
+    prices: [
       "Private Classes",
-      "Choose your class!",
+      "Prices",
       "60 min - 700 Kč",
       "90 min - 900 Kč",
       "Individual approach, the classes are tailored to your needs.",
       "90 min classes are suitable for 2 people."
+    ],
+    about: [
+      "About Me",
+      "My name is Anastasia and my path to yoga has begun 13 years ago when my mother brought me to ashtanga vinyasa class, so I could improve my scoliosis. It wasn't love at first sight. But that's pretty usual with yoga. At the beginning you simply don't believe thta downward facing dog is a relaxing pose. :)",
+      "I am not rigid nor in my own practice, neither in my teaching. I am fascinated by different styles, yoga anatomy, working with breath and energy, meditation techniques and last, but not least, philosophy and mythology.",
+      "I have started to teach in 2019 after completing my yoga teacher training in Ashiyana yoga school  (http://www.ashiyanayogaschool.in/) in India. What I love about teaching is individual work with my students, adjustments and creation of safe nurturing space. That's why I teach individual classes and tiny groups."
     ],
     contact: [
       "/en#contact",
@@ -100,7 +114,7 @@ exports.getEnContent = function() {
       "Subject",
       "Type your message here...",
       "Send",
-      "Sent !"
+      "Sent!"
     ],
     footer: "Website made by"
   };
